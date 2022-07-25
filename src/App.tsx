1fc82743b5f2753fde646c494e3923e235084843
Alysa Meng <menga@cs.washington.edu>
2022-07-24 19:46:26 -0700

    adding links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import './TextArea'
 import texts from './description';
+import pms from "./haikus";
 import './App.css';
-import {FaLinkedin, FaGithub, FaInstagram} from 'react-icons/fa';
+import {FaLinkedin, FaGithub, FaInstagram, FaExternalLinkAlt, FaLightbulb, FaCoffee} from 'react-icons/fa';
 import profile from './profile_pic.jpg';
+import bird from './bird.jpeg';
 import TextArea from './TextArea';
 
 interface AppState {
@@ -24,15 +26,18 @@ export class App extends Component<{}, AppState> {
   render() {
     return (
       <div className="App">
+
         <div className="header">
           <button onClick={() => this.setLang(false)}>EN</button>
           <button onClick={() => this.setLang(true)}>ES</button>
         </div>
-      {this.getIntro()}
-      {this.getAbout()}
-      {this.getWriting()}
-      {this.getProjects()}
-      {this.getFooter()}
+
+        {this.getIntro()}
+        {this.getAbout()}
+        {this.getWriting()}
+        {this.getProjects()}
+        {this.getButton()}
+        {this.getFooter()}
       </div>
     );
   }
@@ -49,7 +54,6 @@ export class App extends Component<{}, AppState> {
         {TextArea("Alicia", "Alysa", this.state.lang)}
         <img src={profile} alt="Profile"/>
       </div>
-
     );
 
   }
@@ -59,9 +63,8 @@ export class App extends Component<{}, AppState> {
     return (
       <div className="about">
         {TextArea("Sobre mí", "About me", this.state.lang)}
-        <p>{texts[this.state.lang === true ? 0 : 1]}</p>
+        <p>{texts[this.state.lang ? 0 : 1]}</p>
       </div>
-
     );
   }
 
@@ -73,27 +76,32 @@ export class App extends Component<{}, AppState> {
 
         <div className="work">
           <h4>"Los perros alegóricos en la Revolución mexicana"</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 2]}</p>
+          <p>{texts[(this.state.lang ? 0 : 1) + 2]}</p>
+          <a className="button" href={process.env.PUBLIC_URL + '/work_files/Perros_alegoricos.pdf'} target='_blank' rel="noreferrer"><FaExternalLinkAlt/></a>
         </div>
 
         <div className="work">
           <h4>"Dibujos de tiza"</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 4]}</p>
+          <p>{texts[(this.state.lang ? 0 : 1) + 4]}</p>
+          <a className="button" href={process.env.PUBLIC_URL + '/work_files/Dibujos_de_tiza.pdf'} target='_blank' rel="noreferrer"><FaExternalLinkAlt/></a>
         </div>
 
         <div className="work">
           <h4>"Carta de presentación escrita por un pícaro"</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 6]}</p>
+          <p>{texts[(this.state.lang ? 0 : 1) + 6]}</p>
+          <a className="button" href={process.env.PUBLIC_URL + '/work_files/Carta_de_presentacion.pdf'} target='_blank' rel="noreferrer"><FaExternalLinkAlt/></a>
         </div>
 
         <div className="work">
           <h4>"Un día en el andén"</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 8]}</p>
+          <p>{texts[(this.state.lang ? 0 : 1) + 8]}</p>
+          <a className="button" href="https://revistaliteral.com/2020/04/13/un-dia-en-el-anden/" target='_blank' rel="noreferrer"><FaExternalLinkAlt/></a>
         </div>
 
         <div className="work">
-          <h4>"Las naranjas sanguinas"</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 10]}</p>
+          <h4>"Entre las naranjas sanguinas"</h4>
+          <p>{texts[(this.state.lang ? 0 : 1) + 10]}</p>
+          <a className="button" href={process.env.PUBLIC_URL + '/work_files/Naranjas_sanguinas.pdf'} target='_blank' rel="noreferrer"><FaExternalLinkAlt/></a>
         </div>
       </div>
     );
@@ -106,25 +114,72 @@ export class App extends Component<{}, AppState> {
       {TextArea("Proyectos", "Projects", this.state.lang)}
       <div className="work">
           <h4>Lingüística computacional con los textos góticos - Java</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 12]}</p>
+          <p>{texts[(this.state.lang ? 0 : 1) + 12]}</p>
+        <a className="button" href="https://aliciaescribe.com"><FaGithub/></a>
         </div>
 
         <div className="work">
           <h4>Clasificación de dialectos - Java</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 14]}</p>
+          <p>{texts[(this.state.lang ? 0 : 1) + 14]}</p>
+          <a className="button" href="https://aliciaescribe.com"><FaGithub/></a>
         </div>
 
         <div className="work">
           <h4>Sitio personal - JavaScript</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 16]}</p>
+          <p>{texts[(this.state.lang ? 0 : 1) + 16]}</p>
+          <a className="button" href="https://github.com/kpwce/site" target='_blank' rel="noreferrer"><FaGithub/></a>
         </div>
 
         <div className="work">
           <h4>Haikus aleatorios - JavaScript</h4>
-          <p>{texts[(this.state.lang === true ? 0 : 1) + 18]}</p>
+          <p>{texts[(this.state.lang ? 0 : 1) + 18]}</p>
+          <button onClick={() => alert(pms[Math.floor(Math.random() * pms.length)])}><FaLightbulb/></button>
         </div>
     </div>
     );
+  }
+
+  getButton() {
+    return(
+        <div className="mystery">
+        {TextArea("¿Qué hace este botón?", "What does this button do?", this.state.lang)}
+        <button id="mystery-button" onClick={() => this.mystery()}><FaCoffee/></button><br/><br/>
+          <iframe id="vid" width="320" height="240" src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&amp"
+                  title="YouTube video player" frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen></iframe>
+          <img id="pic" src={bird} alt="Chill pigeon"/>
+        </div>
+    );
+  }
+
+  // determines function of mystery button
+  mystery() {
+    let mobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+    if (!mobile) {
+      let elem = document.getElementById("vid");
+      if (elem) {
+        if (elem.style.display === "inline-block") {
+          elem.style.display = "none";
+          elem.setAttribute("src", "https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&amp");
+        } else {
+          elem.setAttribute("src", "https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&amp;autoplay=1");
+          setTimeout(function(){if (elem) elem.style.display = "inline-block";},1000);
+        }
+      }
+    } else {
+      let elem = document.getElementById("pic");
+      if (elem) {
+        if (elem.style.display === "inline-block") {
+          elem.style.display = "none";
+        } else {
+          elem.style.display = "inline-block";
+        }
+      }
+
+    }
+
   }
 
   // the awesome footer
---
+++ src/haikus.tsx
@@ -0,0 +1,3 @@
+let pms = ["que no te rindes \nque encuentres la llave \nque ya no llores", "ido del aire\nen su camisa blanca \ncuando lo hice"];
+
+export default pms;
